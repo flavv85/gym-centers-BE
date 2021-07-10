445a9abea41b108aabeb4743139989d7de8bc4cf
FlaviusGorgan <gorganflavius@gmail.com>
2021-07-10 19:35:20 +0300

    Initial commit & DB access config

---
+++ src/main/java/com/flavius/gymcenter/GymCentersApplication.java
@@ -0,0 +1,13 @@
+package com.flavius.gymcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymCentersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymCentersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flavius/gymcenter/GymCentersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flavius.gymcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymCentersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
